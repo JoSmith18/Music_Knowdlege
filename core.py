5f6593804f7951db18fc76abbbc16a02a944792e
Jo'Tavious Smith <jsmith@basecampcodingacademy.org>
2017-09-12 11:20:42 -0500

    Made class of Genres and have a dict of data

--- core.py
+++ core.py
@@ -1,38 +1,62 @@
-import pendulum
-class Items():
-    def __init__(self, inventory):
-        self.inventory = inventory
-        self.bid = 0
-
-    def __repr__(self):
-        return 'Items(Category: '.format(self.inventory.keys())
-
-    def __str__(self):
-        menu = ''
-        for items in self.inventory.keys():
-            menu += '\n|{}|\n'.format(items)
-        return menu
-
-    def get_brands(self, cat):
-        menu = ''
-        if cat in self.inventory.keys():
-            for items in self.inventory[cat].keys():
-                menu += '\n|{}|\n'.format(items)
-        return menu
-
-    def get_product(self, cat, brand):
-        menu = ''
-        if brand in self.inventory[cat].keys():
-            for items in self.inventory[cat][brand]:
-                menu += '\n|{}|\n'.format(items)
-        return menu
+from random import choice
 
 
-class Bidded_Items():
-    def __init__(self, item, bid):
-        self.item = item
-        self.bid = bid
-        self.time = pendulum.
+class MusicWorld():
+    def __init__(self, genres):
+        ''' Dictionary(Genre) '''
+        self.genres = genres
+        self.points = 0
+
+    def add_artist(self, genre, artist, songs):
+        if genre in self.genres.keys():
+            self.genres[genre][artist] = songs
+        else:
+            self.genres[genre] = {artist: songs}
+
+    def add_songs(self, songs):
+        for song in songs:
+            self.songs.append(song)
+
+    # def __repr__(self):
+    #     return 'Genre:{}(Artist:{}, Songs:{})'.format(self.genre, self.artist,
+    #                                                   self.songs)
 
     def __str__(self):
-        return 'Item: {}, Bid: {}, Time: {}'
+        s = ''
+        for genre in self.genres:
+            s += '\nGenre: {}\nArists:\n'.format(genre)
+            for artist in self.genres[genre]:
+                s += '{} - {}\n'.format(artist,
+                                        ', '.join(self.genres[genre][artist]))
+        return s
+
+    def get_song(self, genre):
+        artists = self.genres[genre]
+        l = []
+        for artist in artists.keys():
+            songs = artists[artist]
+            for song in songs:
+                l.append(song)
+        return choice(l)
+
+    def my_artists(self, genre):
+        return '||'.join(self.genres[genre].keys())
+
+    def is_correct(self, answer, song, genre, points):
+        if song in self.genres[genre][answer]:
+            self.genres[genre][answer].remove(song)
+            self.points += points
+            return True
+        return False
+
+    def keep_going(self, genre):
+        artists = self.genres[genre]
+        l = []
+        for artist in artists.keys():
+            songs = artists[artist]
+            for song in songs:
+                l.append(song)
+        return l
+
+    def get_genres(self):
+        return '\n'.join(self.genres.keys())
